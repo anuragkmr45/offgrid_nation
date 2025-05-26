7832a88fe6ea39e8bf59451e2356ae498eaa1eaa
anuragkumarcars24 <anurag.kumar3@cars24.com>
2025-05-26 07:03:06 +0530

    code cleanup

--- lib/features/root/domain/entities/notification_entity.dart
+++ lib/features/root/domain/entities/notification_entity.dart
@@ -1,29 +1,3 @@
-// import 'package:equatable/equatable.dart';
-
-// class NotificationEntity extends Equatable {
-//   final String id;
-//   final String type;
-//   final String? fromUserId;
-//   final String? toUserId;
-//   final String entityId;
-//   final Map<String, dynamic>? meta;
-//   final bool read;
-//   final DateTime createdAt;
-
-//   const NotificationEntity({
-//     required this.id,
-//     required this.type,
-//     required this.fromUserId,
-//     required this.toUserId,
-//     required this.entityId,
-//     required this.meta,
-//     required this.read,
-//     required this.createdAt,
-//   });
-
-//   @override
-//   List<Object?> get props => [id, type, fromUserId, toUserId, entityId, meta, read, createdAt];
-// }
 // 📁 lib/features/notification/domain/entities/notification_entity.dart
 
 import 'package:equatable/equatable.dart';
